505165180db3ad15fb8c5bc0b33c68d60108ea0c
RigoSaid <said.elkarimi@gmail.com>
2021-08-26 01:37:58 +0200

    First Test with mock

---
+++ src/test/java/com/medhead/EmergencyApplicationTest.java
@@ -0,0 +1,28 @@
+package com.medhead;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith( SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmergencyApplicationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void getsAllCall() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/call")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+}
